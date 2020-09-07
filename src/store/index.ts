9af02c2978fa330f1f9a14ee3ab725781a260fc5
TrayHard <wwwtrayfrost@gmail.com>
2020-09-07 13:28:28 +0300

    Added typed store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,19 @@
 import { createStore } from 'vuex';
 
-export default createStore({
-  state: {
-  },
+export type State = {
+  counter: number;
+};
+
+const state: State = {
+  counter: 0,
+};
+
+export const store = createStore({
+  state,
   mutations: {
+    increment(state) {
+      state.counter += 1;
+    },
   },
   actions: {
   },
